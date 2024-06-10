2103d730cc5c84a806aebda4dd3b9a56e00e9e41
Paramon, Yevstigneyev <90145033+PYP2205@users.noreply.github.com>
2024-06-10 17:24:20 +0300

    Update 'gen_keys.sh'
    Changed how private and public keys are generated. Now the script generates pre-shared keys, and checks for WireGuard on the user's system. Which if not present would show the user how to install WireGuard onto their system.

--- gen_keys.sh
+++ gen_keys.sh
@@ -1,15 +1,19 @@
 # Linux Shell script that generates WireGuard Keys
 
 # Generates Public and Private Keys
-wg genkey | tee privatekey | wg pubkey > publickey
-echo
+private_key=`wg genkey`
+public_key=`echo $private_key | wg pubkey`
+preshared_key=`wg genpsk`
 
-# Shows generated Public key
-echo Public Key:
-cat publickey
+if [[ `command -v wg` == "" && `command -v wg-quick` == "" ]] then
+    echo "Wireguard is not installed on your system."
+    echo "For Debian and Debian-Based systems: apt install wireguard-tools"
+    echo "For RHEL and RHEL-Based systems: dnf install wireguard-tools"
+    echo "For Arch and Arch-Based systems: pacman -S wireguard-tools"
+    exit 1
+fi
 
-echo
-
-# Shows generated Private key
-echo Private Key:
-cat privatekey
+echo "Public Key: $public_key"
+echo "Private Key: $private_key"
+echo "Pre-shared Key: $preshared_key"
+exit 0
